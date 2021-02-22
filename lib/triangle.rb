80069177c94a7cc62aee4df7a4fbc4f077a04f84
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-02-22 17:04:14 -0500

    commit tree e3e667ec2fe888de0e62eabd9fe2f3642f67a694

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    illegal
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
+    else a != b && b != c && a != c
+      :scalene
+    end 
+  end
+
+  # def illegal
+  #   triangle = [(l1 + l2 > l3), (l1 + l3 > l2), (l2 + l3 > l1)]
+  #   [l1, l2, l3].each do |side|
+  #     if side <= 0
+  #       false
+  #     raise Triangle::TriangleError if Triangle.include?(false)
+  #     end
+  #   end
+  # end
+  def illegal
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each do |side|
+        binding.pry
+        real_triangle << false if side <= 0
+        raise Triangle::TriangleError if real_triangle.include?(false)
+      end
+    end
+
+  class TriangleError < StandardError
+  end
+  
 end
